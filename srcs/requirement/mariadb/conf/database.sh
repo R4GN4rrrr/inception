aa971b29606f333aa102ce6d654dff8f664327e8
R4GN4rrrr <ymenyoub@student.1337.ma>
2024-11-05 08:54:31 -0500

    mariadb is working , now working on wordpress

--- srcs/requirement/mariadb/conf/database.sh
+++ srcs/requirement/mariadb/conf/database.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 service mariadb start
-
+sleep 5
 mariadb -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;"
 mariadb -e "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' ;"
 mariadb -e "GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' ;"
